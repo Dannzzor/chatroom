f4bc71f1224ca98d7e352b1d7223f76a4c363d15
Danny Davenport <danny@firma8.com>
2015-07-09 10:03:53 -0400

    added semantic ui

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -22,8 +22,25 @@ function join () {
     });
     
     socket.on('private message', function(from, msg){
-
-        $('#p-messages').append($('<li>').text(from + ': ' + msg));
+        var msgEl = $('<div class="event">'+
+              '<div class="label">'+
+                '<img src="/images/Cat.png">'+
+              '</div>'+
+              '<div class="content">'+
+                '<div class="summary">'+
+                  msg +
+                  '<div class="date">'+
+                    '1 Hour Ago'+
+                  '</div>'+
+                '</div>'+
+                '<div class="meta">'+
+                  '<a class="like">'+
+                    '<i class="like icon"></i>'+ (Math.floor(Math.random(10)*10)) +' Likes'+
+                  '</a>'+
+                '</div>'+
+              '</div>'+
+            '</div>');
+        $('#p-messages').append(msgEl);
     });
 
     $usr.hide();
